81af57aec3c98d0b2ade1161823e8c155642b32b
hyunsu joo <hs9880@gmail.com>
2019-06-13 08:37:52 -0400

    add gh page

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,17 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Search from "./Search";
 import "./Header.css";
+import logo from "./bitcoin_logo.png";
 
 function Header() {
   const title = "CoinTracker";
   return (
     <header>
       <Link to="/" className="header-logo">
-        <img
-          src="/image/bitcoin_logo.png"
-          alt="bitcoin_logo"
-          className="bitcoin-logo"
-        />
+        <img src={logo} alt="bitcoin_logo" className="bitcoin-logo" />
         <div className="header-title">{title}</div>
       </Link>
       <Search />
